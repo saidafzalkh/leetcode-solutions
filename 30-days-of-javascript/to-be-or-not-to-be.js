9ed03e971fabc1b3afcb89bdaa4df167a889c7e9
Saidafzal (Sayid) Kholkhujaev <saidafzal1209@gmail.com>
2025-05-17 14:57:30 +0500

    problem #3

---
+++ 30-days-of-javascript/to-be-or-not-to-be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val !== expected) throw new Error('Not Equal');
+            return true;
+        },
+        notToBe: function(expected) {
+            if (val === expected) throw new Error('Equal');
+            return true;
+        }
+    };
+};
