bc82911348642f120e079570557380bf5a9407d1
Saidafzal (Sayid) Kholkhujaev <saidafzal1209@gmail.com>
2025-05-25 03:58:23 +0500

    problem #14

---
+++ 30-days-of-javascript/cancellation.ts
@@ -0,0 +1,9 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+    const timeoutId = setTimeout(() => {fn(...args)}, t);
+    return function() {
+        clearTimeout(timeoutId);
+    }
+};
